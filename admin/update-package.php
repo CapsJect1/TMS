39c9a7cfb8fd01e0d3665ba77d5255eaa2bf46f5
johnrey alo <johnreyalo@gmail.com>
2024-08-01 01:23:00 +0800

    commit

--- admin/update-package.php
+++ admin/update-package.php
@@ -1,6 +1,8 @@
 <?php
-session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+session_start();
 include('includes/config.php');
 if (strlen($_SESSION['alogin']) == 0) {
 	header('location:index.php');
