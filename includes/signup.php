5c81c1843c70f45b01ffe3b432c3408040c7dee7
CapsJect1 <caburnayreyjohn4@gmail.com>
2024-11-12 06:31:56 +0800

    commit

--- includes/signup.php
+++ includes/signup.php
@@ -8,7 +8,7 @@ use PHPMailer\PHPMailer\SMTP;
 require "./phpmailer/src/Exception.php";
 require "./phpmailer/src/PHPMailer.php";
 require "./phpmailer/src/SMTP.php";
-error_reporting(0);
+error_reporting(E_ALL);
 if (isset($_POST['submit'])) {
 	$verification = uniqid() . rand(100, 999999999);
 	$fname = $_POST['fname'];
