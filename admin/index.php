4a9fa11b5187138ffdd93b4631146b2001b86f63
CapsJect1 <caburnayreyjohn4@gmail.com>
2024-11-30 11:44:22 +0800

    commit

--- admin/index.php
+++ admin/index.php
@@ -164,6 +164,14 @@ if (isset($_POST['login'])) {
 		}
 	}
 </script>
+<script>
+document.addEventListener("DOMContentLoaded", function(){
+  const newUrl = 'admin/';
+
+// Change the URL without refreshing the page
+history.pushState(null, '', newUrl);
+})
+</script>
 </body>
 
 </html>
