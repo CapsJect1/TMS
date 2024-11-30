aef4a1c95498cfe4a3003b5ea6920a289d2fa81b
CapsJect1 <caburnayreyjohn4@gmail.com>
2024-11-30 11:20:19 +0800

    commit

--- admin/includes/layout-foot.php
+++ admin/includes/layout-foot.php
@@ -9,13 +9,5 @@
 
 <script src="js/sweet_alert.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" ></script>
-<script>
-document.addEventListener("DOMContentLoaded", function(){
-  const newUrl = '/';
-
-// Change the URL without refreshing the page
-history.pushState(null, '', newUrl);
-})
-</script>
 </body>
 </html>
