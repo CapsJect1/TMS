42ef137b424f642ba3da339c27c6f58605b78b8f
CapsJect1 <caburnayreyjohn4@gmail.com>
2024-12-05 03:39:52 +0800

    commit

--- ticket.php
+++ ticket.php
@@ -297,14 +297,14 @@ if (isset($_POST['submit'])) {
         <br>
         <!-- <a href="cart.php" class="btn btn-secondary">Go to Cart</a> -->
     </div>
-    <script>
+    <!-- <script>
 document.addEventListener("DOMContentLoaded", function(){
   const newUrl = '/';
 
 // Change the URL without refreshing the page
 history.pushState(null, '', newUrl);
 })
-</script>
+</script> -->
 </body>
 
 </html>
