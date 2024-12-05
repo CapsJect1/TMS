e674114af84d563a41e9c20348c7d4cc79d44ca2
CapsJect1 <caburnayreyjohn4@gmail.com>
2024-12-05 19:47:01 +0800

    Update signin.php

--- includes/signin.php
+++ includes/signin.php
@@ -1,76 +1,3 @@
-<?php
-
-if (isset($_POST['signin'])) {
-	$email = htmlspecialchars(stripslashes(trim($_POST['email'])));
-	// $password = md5($_POST['password']); // Note: MD5 hashing is used here for simplicity; consider using more secure hashing methods
-	$password = htmlspecialchars(stripslashes(trim($_POST['password'])));
-	$status = 2;
-
-	// SQL query to fetch user details based on email and password
-	$sql = "SELECT id, FullName, EmailId, fname, lname,Password, Status FROM tblusers WHERE EmailId=:email AND Status = :stat";
-	$query = $dbh->prepare($sql);
-	$query->bindParam(':email', $email, PDO::PARAM_STR);
-	// $query->bindParam(':password', $password, PDO::PARAM_STR);
-	$query->bindParam(':stat', $status, PDO::PARAM_INT);
-	$query->execute();
-	$user = $query->fetch(PDO::FETCH_ASSOC);
-
-	if ($query->rowCount() > 0) {
-		if ($user['Status'] == 2) {
-			// Set session variables upon successful login
-
-			if (password_verify($password, $user['Password'])) {
-				$_SESSION['user_id'] = $user['id'];
-				$_SESSION['user_name'] = $user['FullName'];
-				$_SESSION['login'] = $user['EmailId'];
-				$_SESSION['fname'] = $user['fname'];
-				$_SESSION['lname'] = $user['lname'];
-				// Redirect to a dashboard or home page after successful login
-				// header("Location: package-list.php");\
-				?>
-				<script>
-					window.location.href = "package-list.php"
-				</script>
-				<?php
-			} else {
-				echo "<script>
-					Swal.fire({
-						title: 'Error!',
-						text: 'Incorrect email or password',
-						icon: 'error',
-						timer: 1500,
-						showConfirmButton: false
-					});
-					</script>";
-			}
-		} else {
-			echo "<script>
-			Swal.fire({
-				title: 'Error!',
-				text: 'Please confirm your account first',
-				icon: 'error',
-				timer: 1500,
-				showConfirmButton: false
-			});
-			</script>";
-		}
-
-		exit;
-	} else {
-		echo "<script>
-			Swal.fire({
-				title: 'Error!',
-				text: 'Please confirm your account first',
-				icon: 'error',
-				timer: 1500,
-				showConfirmButton: false
-			});
-			</script>";
-	}
-}
-?>
-
-<!-- HTML form for login -->
 <div class="modal fade" id="myModal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 	<div class="modal-dialog" role="document">
 		<div class="modal-content modal-info">
@@ -85,11 +12,16 @@ if (isset($_POST['signin'])) {
 							<form method="post" name="login">
 								<h3>Sign in with your account</h3>
 								<input type="text" name="email" id="email" placeholder="Enter your Email" required="">
+
 								<div style="position: relative;">
 									<input type="password" name="password" id="password" placeholder="Password" value=""
 										required="">
 									<i class="fa fa-eye" id="show-pass2" style="position: absolute; top: 0; right: 0; margin: 35px 10px 0 0;"></i>
 								</div>
+
+								<!-- reCAPTCHA -->
+								<div class="g-recaptcha" data-sitekey="your-site-key-here"></div>
+
 								<h4><a href="forgot-password.php">Forgot password</a></h4>
 								<input type="submit" name="signin" value="SIGN IN">
 							</form>
@@ -103,18 +35,3 @@ if (isset($_POST['signin'])) {
 		</div>
 	</div>
 </div>
-
-<script>
-	let showPass2 = document.getElementById('show-pass2');
-	showPass2.onclick = () => {
-		let passwordInp = document.forms['login']['password'];
-		if (passwordInp.getAttribute('type') == 'password') {
-			showPass2.classList.replace('fa-eye', 'fa-eye-slash')
-
-			passwordInp.setAttribute('type', 'text')
-		} else {
-			showPass2.classList.replace('fa-eye-slash', 'fa-eye')
-			passwordInp.setAttribute('type', 'password')
-		}
-	}
-</script>
