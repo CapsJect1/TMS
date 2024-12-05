b8faa90c19be55659279f42a06f0a15b2f3e779c
CapsJect1 <caburnayreyjohn4@gmail.com>
2024-12-05 21:08:48 +0800

    Update index1.php

--- index1.php
+++ index1.php
@@ -63,8 +63,7 @@ include ('includes/config.php');
 					<a href="package-list.php" class="btn btn-primary" style="color: #fff !important; margin-top: 15px;">Book Now</a>
 				</div>
 				<!-- <div class="col-md-6" >
-				<img src="./images/port.webp
-" alt="hero-img-1">
+				<img src="./images/port.webp" alt="hero-img-1">
 				</div> -->
 				
 			</div>
