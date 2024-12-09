4a97b68b154b4faa5075cdcb3703e6cde87d0d07
CapsJect1 <caburnayreyjohn4@gmail.com>
2024-12-09 18:13:44 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -112,9 +112,13 @@ if (isset($_POST['login'])) {
 
 
 
+
+
+
 <div class="container justify-content-center align-items-center d-flex ">
-	<div class="shadow p-4">
-    <form method="post" name="login" style="max-width: 600px; width: 100%;">
+	<div class="">
+   <div class="card p-5">
+	<form method="post" name="login" style="max-width: 600px; width: 100%;">
 		<a  href="../index.php" class="bg">
 			<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="" class="bi bi-arrow-left-square-fill" viewBox="0 0 16 16">
 				<path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1"/>
@@ -144,9 +148,12 @@ if (isset($_POST['login'])) {
 		</div>
 		</div>
     </form>
+   </div>
 	</div>
 
 </div>
+
+	
 <script>
 	let showPass = document.getElementById('show-pass');
 	showPass.onclick = () => {
