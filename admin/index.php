4705d8248cc4f72d52a941bd5149415c29d3d822
CapsJect1 <caburnayreyjohn4@gmail.com>
2024-12-09 18:25:22 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -120,9 +120,9 @@ if (isset($_POST['login'])) {
    <div class="card p-4">
 	<form method="post" name="login" style="max-width: 600px; width: 100%;">
 		<a  href="../index.php" class="bg">
-			<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="" class="bi bi-arrow-left-square-fill" viewBox="0 0 16 16">
-				<path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1"/>
-			  </svg>
+			<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#38AF05" class="bi bi-arrow-left" viewBox="0 0 16 16">
+				<path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+			</svg>
 		</a>
 		<h1 class="mb-4 text-primary text-center">
 			Sign in
