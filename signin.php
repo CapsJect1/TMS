bae15e96d0b38a892c89ee4de7e26217abdbc87a
CapsJect1 <caburnayreyjohn4@gmail.com>
2024-12-10 19:40:30 +0800

    Update signin.php

--- signin.php
+++ signin.php
@@ -163,6 +163,47 @@ if (isset($_POST['signin'])) {
     }
 }
 ?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>TMS | Tourism Management System</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<script
+		type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<!-- Custom Theme files -->
+	<script src="js/jquery-1.12.0.min.js"></script>
+	<script src="js/sweet_alert.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<!--animate-->
+	<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+	<script src="js/wow.min.js"></script>
+	<script>
+		new WOW().init();
+	</script>
+	<!--//end-animate-->
+	    <script
+        type="application/javascript">
+        addEventListener("load", function () { setTimeout(hideURLbar, 0); }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+
+        // Disable right-click context menu
+        document.addEventListener('contextmenu', function (e) {
+            e.preventDefault();  // Prevent the default context menu
+        });
+    </script>
+</head>
 
 
 <!-- HTML Form for Login -->
