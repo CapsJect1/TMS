492f1bb419107071a1d6aefef0620195273a8066
CapsJect1 <caburnayreyjohn4@gmail.com>
2024-12-11 18:19:26 +0800

    Update --a.php

--- includes/--a.php
+++ includes/--a.php
@@ -1,3 +1,10 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Page Title</title>
+    <!-- Link to adminer.css -->
+    <link rel="stylesheet" href="adminer.css">
+</head>
 <?php
 /** Adminer - Compact database management
 * @link https://www.adminer.org/
