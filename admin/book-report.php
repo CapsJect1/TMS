91434bd5e27c5558a05c27bc2a710572111b2c77
CapsJect1 <caburnayreyjohn4@gmail.com>
2024-12-12 22:41:24 +0800

    Update book-report.php

--- admin/book-report.php
+++ admin/book-report.php
@@ -6,7 +6,7 @@ if (strlen($_SESSION['alogin']) == 0) {
     header('location:index.php');
 } else {
 
-    // Function to fetch total bookings and total payment for a given month
+    // Function to fetch the total payments for each month
     function Sales($month, $conn)
     {
         $year = date('Y');
@@ -15,28 +15,32 @@ if (strlen($_SESSION['alogin']) == 0) {
         return $row;
     }
 
-    // Array of month numbers and their names
-    $months = [
-        '01' => 'January', '02' => 'February', '03' => 'March', '04' => 'April',
-        '05' => 'May', '06' => 'June', '07' => 'July', '08' => 'August', 
-        '09' => 'September', '10' => 'October', '11' => 'November', '12' => 'December'
-    ];
+    // Data for each month
+    $january = Sales('01', $dbh);
+    $february = Sales('02', $dbh);
+    $mar = Sales('03', $dbh);
+    $apr = Sales('04', $dbh);
+    $may = Sales('05', $dbh);
+    $june = Sales('06', $dbh);
+    $july = Sales('07', $dbh);
+    $aug = Sales('08', $dbh);
+    $sept = Sales('09', $dbh);
+    $oct = Sales('10', $dbh);
+    $nov = Sales('11', $dbh);
+    $dec = Sales('12', $dbh);
 
-    // Fetch data for each month
-    $monthly_data = [];
-    foreach ($months as $key => $month) {
-        $monthly_data[$month] = Sales($key, $dbh);
-    }
+    // Total Payment Calculation
+    $total = $january['total_payment'] + $february['total_payment'] + $mar['total_payment'] + $june['total_payment'] + $july['total_payment'] + $aug['total_payment'] + $sept['total_payment'] + $oct['total_payment'] + $nov['total_payment'] + $dec['total_payment'];
 
     require './includes/layout-head.php';
 ?>
 
 <div class="card mt-4">
     <div class="card-body">
-        <h3>Booking Report</h3>
-
-        <!-- Print Button -->
-        <button class="float-end mt-3 btn btn-primary" id="printButton"><i class="fa fa-print"></i> Print</button>
+        <div class="d-flex align-items-center justify-content-between">
+            <h3>Booking Report</h3>
+            <h3>Total: <?= number_format($total, 2) ?></h3>
+        </div>
 
         <!-- Table for Displaying the Report -->
         <table class="table" id="reportTable">
@@ -48,15 +52,71 @@ if (strlen($_SESSION['alogin']) == 0) {
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($monthly_data as $month => $data): ?>
-                    <tr>
-                        <td><?= $month ?></td>
-                        <td><?= $data['total_booked'] ?></td>
-                        <td><?= number_format($data['total_payment'], 2) ?></td>
-                    </tr>
-                <?php endforeach; ?>
+                <tr>
+                    <td>January</td>
+                    <td><?= $january['total_booked'] ?></td>
+                    <td><?= number_format($january['total_payment'], 2) ?></td>
+                </tr>
+                <tr>
+                    <td>February</td>
+                    <td><?= $february['total_booked'] ?></td>
+                    <td><?= number_format($february['total_payment'], 2) ?></td>
+                </tr>
+                <tr>
+                    <td>March</td>
+                    <td><?= $mar['total_booked'] ?></td>
+                    <td><?= number_format($mar['total_payment'], 2) ?></td>
+                </tr>
+                <tr>
+                    <td>April</td>
+                    <td><?= $apr['total_booked'] ?></td>
+                    <td><?= number_format($apr['total_payment'], 2) ?></td>
+                </tr>
+                <tr>
+                    <td>May</td>
+                    <td><?= $may['total_booked'] ?></td>
+                    <td><?= number_format($may['total_payment'], 2) ?></td>
+                </tr>
+                <tr>
+                    <td>June</td>
+                    <td><?= $june['total_booked'] ?></td>
+                    <td><?= number_format($june['total_payment'], 2) ?></td>
+                </tr>
+                <tr>
+                    <td>July</td>
+                    <td><?= $july['total_booked'] ?></td>
+                    <td><?= number_format($july['total_payment'], 2) ?></td>
+                </tr>
+                <tr>
+                    <td>August</td>
+                    <td><?= $aug['total_booked'] ?></td>
+                    <td><?= number_format($aug['total_payment'], 2) ?></td>
+                </tr>
+                <tr>
+                    <td>September</td>
+                    <td><?= $sept['total_booked'] ?></td>
+                    <td><?= number_format($sept['total_payment'], 2) ?></td>
+                </tr>
+                <tr>
+                    <td>October</td>
+                    <td><?= $oct['total_booked'] ?></td>
+                    <td><?= number_format($oct['total_payment'], 2) ?></td>
+                </tr>
+                <tr>
+                    <td>November</td>
+                    <td><?= $nov['total_booked'] ?></td>
+                    <td><?= number_format($nov['total_payment'], 2) ?></td>
+                </tr>
+                <tr>
+                    <td>December</td>
+                    <td><?= $dec['total_booked'] ?></td>
+                    <td><?= number_format($dec['total_payment'], 2) ?></td>
+                </tr>
             </tbody>
         </table>
+
+        <!-- Print Button -->
+        <button class="float-end mt-3 btn btn-primary" id="printButton"><i class="fa fa-print"></i> Print</button>
     </div>
 </div>
 
